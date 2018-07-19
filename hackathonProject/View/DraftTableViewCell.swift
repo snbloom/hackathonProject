2c52d639631f2ff0a24e10c26b1108f251f7127e
Amy Liu <amytheliu13@gmail.com>
2018-07-19 14:12:09 -0700

    added draft cells

---
+++ hackathonProject/View/DraftTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DraftTableViewCell.swift
+//  hackathonProject
+//
+//  Created by Amy Liu on 7/19/18.
+//
+
+import UIKit
+
+class DraftTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var draftTitle: UILabel!
+    @IBOutlet weak var draftText: UILabel!
+    @IBOutlet weak var draftDate: UILabel!
+    
+}
